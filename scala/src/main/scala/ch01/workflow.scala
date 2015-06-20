58895a97377629250f5968d4507cc8cfc7653b26
Peter Hilton <hilton@users.noreply.github.com>
2015-06-20 17:46:56 +0200

    Separate Workflow and WorkflowInstance models

--- scala/src/main/scala/ch01/workflow.scala
+++ scala/src/main/scala/ch01/workflow.scala
@@ -4,22 +4,11 @@ import java.util.Date
 import scala.collection.mutable.ListBuffer
 
 
-/** A sequence flow from one workflow activity to another. */
-case class Transition(from: Activity, to: Activity)
+/** A process defined by activities (nodes) in the process, and their transitions. */
+case class Workflow(activities: Set[Activity] = Set.empty) {
 
-
-/**
- * A workflow activity (a.k.a. task) that may have nested activities and transitions to other activities. A workflow is
- * just a top-level activity with no transitions. Custom activity types are subclasses that override execute.
- */
-class Activity(val id: String, val activities: Set[Activity] = Set.empty, val transitions: Set[Transition] = Set.empty) {
-
-  protected def copy(id: String = id, activities: Set[Activity] = activities, transitions: Set[Transition] = transitions): Activity = {
-    new Activity(id, activities, transitions)
-  }
-
-  /** Returns a copy of this activity with the transition specified by the pair of activity IDs added.*/
-  def +(transition: (String, String)): Activity = transition match { case(fromId, toId) =>
+  /** Returns a copy of this workflow with the transition specified by the pair of activity IDs added.*/
+  def +(transition: (String, String)): Workflow = transition match { case(fromId, toId) =>
     (for {
       from <- activities.find(_.id == fromId)
       to <- activities.find(_.id == toId)
@@ -29,44 +18,50 @@ class Activity(val id: String, val activities: Set[Activity] = Set.empty, val tr
     }.getOrElse(this)
   }
 
-  /** Starts execution of this activity. */
-  def start(): ActivityInstance = {
-    val activityInstance = ActivityInstance(this)
-    execute(activityInstance)
-    activityInstance
-  }
-
-  /** Executes this activities nested activities, in the context of the given activity instance. */
-  def execute(activityInstance: ActivityInstance): Unit = {
+  /** Starts execution of this workflow, by executing activities with no incoming transition. */
+  def start(): WorkflowInstance = {
+    val workflowInstance = WorkflowInstance(this)
     val toActivities = activities.flatMap(_.transitions).map(_.to)
     val startActivities = activities.toSet.diff(toActivities)
-    startActivities.foreach(startActivity => activityInstance.execute(startActivity))
+    startActivities.foreach(startActivity => workflowInstance.execute(startActivity))
+    workflowInstance
   }
 }
 
 
-/** An execution of an activity, which is an execution scope, with an optional parent execution. */
-case class ActivityInstance(activity: Activity, parent: Option[ActivityInstance] = None,
-  var endTime: Option[Date] = None, activityInstances: ListBuffer[ActivityInstance] = new ListBuffer()) {
+/** A sequence flow from one workflow activity to another. */
+case class Transition(from: Activity, to: Activity)
 
-  val startTime = new Date()
 
-  /** Executes an activity within this execution scope. */
+/** A workflow activity (a.k.a. task) that has transitions to other activities. */
+abstract class Activity(val id: String, val transitions: Set[Transition] = Set.empty) {
+
+  def copy(id: String = id, transitions: Set[Transition] = transitions): Activity
+
+  def execute(activityInstance: ActivityInstance): Unit
+}
+
+
+/** A single case of running a workflow. */
+case class WorkflowInstance(workflow: Workflow, activityInstances: ListBuffer[ActivityInstance] = new ListBuffer()) {
+  
+  /** Executes an activity within this workflow instance. */
   def execute(activity: Activity): Unit = {
-    val activityInstance = ActivityInstance(activity, Some(this))
+    val activityInstance = ActivityInstance(activity, this)
     activityInstances.append(activityInstance)
     activity.execute(activityInstance)
   }
+}
+
+
+/** A single case of running an activity. */
+case class ActivityInstance(activity: Activity, workflowInstance: WorkflowInstance, var endTime: Option[Date] = None) {
+
+  val startTime = new Date()
 
   /** Completes this execution and takes transitions to the next activities. */
   def end(): Unit = {
     endTime = Some(new Date())
-    activity.transitions.foreach(transition => take(transition))
-  }
-
-  /** Takes a transition to (executes) another activity. */
-  def take(transition: Transition): Unit = {
-    endTime = Some(new Date())
-    parent.map(_.execute(transition.to))
+    activity.transitions.foreach(transition => workflowInstance.execute(transition.to))
   }
 }
--- scala/src/test/scala/ExecutionSpec.scala
+++ scala/src/test/scala/ExecutionSpec.scala
@@ -2,11 +2,10 @@ package ch01
 
 import org.scalatest._
 
-case class Automatic(override val id: String, override val activities: Set[Activity] = Set.empty,
-  override val transitions: Set[Transition] = Set.empty) extends Activity(id, activities, transitions) {
+case class Automatic(override val id: String, override val transitions: Set[Transition] = Set.empty) extends Activity(id, transitions) {
 
-  override def copy(id: String = id, activities: Set[Activity] = activities, transitions: Set[Transition] = transitions): Activity = {
-    Automatic(id, activities, transitions)
+  override def copy(id: String = id, transitions: Set[Transition] = transitions): Activity = {
+    Automatic(id, transitions)
   }
 
   override def execute(activityInstance: ActivityInstance): Unit = {
@@ -15,11 +14,10 @@ case class Automatic(override val id: String, override val activities: Set[Activ
   }
 }
 
-case class Wait(override val id: String, override val activities: Set[Activity] = Set.empty,
-  override val transitions: Set[Transition] = Set.empty) extends Activity(id, activities, transitions) {
+case class Wait(override val id: String, override val transitions: Set[Transition] = Set.empty) extends Activity(id, transitions) {
 
-  override def copy(id: String = id, activities: Set[Activity] = activities, transitions: Set[Transition] = transitions): Activity = {
-    Wait(id, activities, transitions)
+  override def copy(id: String = id, transitions: Set[Transition] = transitions): Activity = {
+    Wait(id, transitions)
   }
 
   override def execute(activityInstance: ActivityInstance): Unit = println(s"Wait: executing activity $id")
@@ -28,38 +26,38 @@ case class Wait(override val id: String, override val activities: Set[Activity] 
 class ExecutionSpec extends FlatSpec with Matchers {
 
   "A workflow" should "do sequential automatic execution" in {
-    val workflow = new Activity("workflow", Set(Automatic("a"), Automatic("b"))) + ("a" -> "b")
+    val workflow = new Workflow(Set(Automatic("a"), Automatic("b"))) + ("a" -> "b")
     val workflowInstance = workflow.start()
     assert(openActivities(workflowInstance).isEmpty, "open")
     assert(completedActivities(workflowInstance) == Set("a", "b"), "complete")
   }
 
   it should "do parallel automatic execution" in {
-    val workflow = new Activity("workflow", Set(Automatic("a"), Automatic("b")))
+    val workflow = new Workflow(Set(Automatic("a"), Automatic("b")))
     val workflowInstance = workflow.start()
     assert(openActivities(workflowInstance).isEmpty)
     assert(completedActivities(workflowInstance) == Set("a", "b"))
   }
 
   it should "do sequential wait execution" in {
-    val workflow = new Activity("workflow", Set(Wait("a"), Wait("b"))) + ("a" -> "b")
+    val workflow = new Workflow(Set(Wait("a"), Wait("b"))) + ("a" -> "b")
     val workflowInstance = workflow.start()
     assert(openActivities(workflowInstance) == Set("a"))
     assert(completedActivities(workflowInstance).isEmpty)
   }
 
   it should "do parallel wait execution" in {
-    val workflow = new Activity("workflow", Set(Wait("a"), Wait("b")))
+    val workflow = new Workflow(Set(Wait("a"), Wait("b")))
     val workflowInstance = workflow.start()
     assert(openActivities(workflowInstance) == Set("a", "b"))
     assert(completedActivities(workflowInstance).isEmpty)
   }
 
-  private def openActivities(workflowInstance: ActivityInstance): Set[String] = {
+  private def openActivities(workflowInstance: WorkflowInstance): Set[String] = {
     workflowInstance.activityInstances.filter(_.endTime.isEmpty).map(_.activity.id).toSet
   }
 
-  private def completedActivities(workflowInstance: ActivityInstance): Set[String] = {
+  private def completedActivities(workflowInstance: WorkflowInstance): Set[String] = {
     workflowInstance.activityInstances.filter(_.endTime.isDefined).map(_.activity.id).toSet
   }
 }
